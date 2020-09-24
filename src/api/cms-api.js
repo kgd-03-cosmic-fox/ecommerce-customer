a687e25ae9309544382617756a7a33ab5a6d7fc9
BudiartoSalim <budiarto.salim91@gmail.com>
2020-09-24 12:36:06 +0700

    ready to deploy

--- src/api/cms-api.js
+++ src/api/cms-api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: 'https://nameless-cove-00105.herokuapp.com'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://nameless-cove-00105.herokuapp.com'
 })
--- src/views/MyCart.vue
+++ src/views/MyCart.vue
@@ -1,28 +1,40 @@
 <template>
   <div>
+    <h1>My Cart</h1>
     <table class="table">
       <thead>
         <tr class="row">
-          <th class="col-3">
-          </th>
+          <th class="col-3"></th>
           <th scope="col" class="col-3" style="word-wrap: break-word">Name</th>
-          <th scope="col" class="col-1" style="word-wrap: break-word">Amount</th>
+          <th scope="col" class="col-1" style="word-wrap: break-word">
+            Amount
+          </th>
           <th scope="col" class="col-2">Price</th>
           <th scope="col" class="col-3"></th>
         </tr>
       </thead>
       <tbody>
         <CartContent
-        v-for="content in fetchedContent" :key="content.id"
-        :content="content"
+          v-for="content in fetchedContent"
+          :key="content.id"
+          :content="content"
         >
         </CartContent>
-        <!--         <CMSSidebarContent
-          v-for="product in productList"
-          v-bind:key="product.id"
-          v-bind:product="product"
-        >
-        </CMSSidebarContent> -->
+        <tr class="row">
+          <th class="col-3"></th>
+          <th scope="col" class="col-4" style="word-wrap: break-word">
+            Total Price:
+          </th>
+          <th scope="col" class="col-4">
+            {{
+              new Intl.NumberFormat("de-DE", {
+                style: "currency",
+                currency: "IDR",
+              }).format(totalPrice)
+            }}
+          </th>
+          <th scope="col" class="col-1" style="word-wrap: break-word"></th>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -33,6 +45,13 @@ import CartContent from '../components/CartContent.vue'
 export default {
   components: { CartContent },
   computed: {
+    totalPrice () {
+      let output = 0
+      this.fetchedContent.forEach((el) => {
+        output += el.Product.price * el.amount
+      })
+      return output
+    },
     fetchedContent () {
       return this.$store.state.cartContent
     }
